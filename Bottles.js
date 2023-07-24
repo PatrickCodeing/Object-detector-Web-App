6bd67d5ef9c6099c6bb394a063282e184206ef7d
PatrickCodeing <117385304+PatrickCodeing@users.noreply.github.com>
2023-07-24 18:30:32 +0100

    Add files via upload

---
+++ Bottles.js
@@ -0,0 +1,27 @@
+img = "";
+status1 = "";
+objects = [];
+
+function preload(){
+    img = loadImage('Bottles.jpg');
+}
+
+function setup(){
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detected Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded");
+    status1 = true;
+}
+
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
