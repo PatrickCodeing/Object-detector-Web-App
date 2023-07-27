964d0ea9ad85aec5376ce5a87c4ab29c60311c78
PatrickCodeing <117385304+PatrickCodeing@users.noreply.github.com>
2023-07-27 12:53:07 +0100

    Add files via upload

--- Desk.js
+++ Desk.js
@@ -6,11 +6,33 @@ function preload(){
     img = loadImage('Desk.jpg');
 }
 
+function back(){
+    window.location = "index.html";
+}
+
 function setup(){
     canvas = createCanvas(640, 420);
     canvas.center();
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML = "Status : Detected Objects";
+}
+
+function draw(){
+    image(img, 0, 0, 640, 420);
+
+    if(status1 != ""){
+
+        for( i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Status: Object Detected";
+            document.getElementById("object_num").innerHTML = "Number of objects detected are : "+objects.length;
+            fill("red");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("red");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    } 
 }
 
 function modelLoaded(){
